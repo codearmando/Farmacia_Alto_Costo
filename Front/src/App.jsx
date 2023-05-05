9653bdc26a175973d5081377cf14592c85657616
0.g <armandowork36@gmail.com>
2023-05-05 08:13:07 -0400

    modulos ticket,medicamento,inventairomedicamento,paciente

---
+++ Front/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Rutas from './Routers/Rutas'
+// import Viewpaciente from './Components/Pacientes/Viewpaciente'
+
+
+function App() {
+  return (
+    <>
+      {/* <Viewpaciente /> */}
+      <Rutas></Rutas>
+    </>
+  )
+}
+
+export default App
